bbcf4bca82577087433af619cfca2f8c50e926e9
connor-robinson <ansonchanw@gmail.com>
2025-12-17 18:32:36 +0800

    Configure Next.js to ignore ESLint errors during builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
   transpilePackages: ['@supabase/supabase-js'],
 
   experimental: {
